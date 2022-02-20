5995554b4983c3051dc9a6e919d3cb74d886fc6a
Asma <as1588ma@gmail.com>
2022-02-19 21:02:07 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import Weather from "./Weather";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="Paris" />
+        <footer className="text-muted mt-3">
+          This project was coded by{" "}
+          <span className="myName">Asma Sadiq</span> and is{" "}
+          <a
+            href="https://github.com/Elodie-n/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://romantic-mclean-b963a4.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
